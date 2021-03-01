65f4c6e2ba666b28c95fe9a67735d7902742e3bb
Ryoto-Noguchi <ryoto.noguchi@gmail.com>
2021-03-02 07:27:08 +0900

    React入門#4 完了

--- src/Article.jsx
+++ src/Article.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 const Article = (props) => {
+
   return (
     <React.Fragment>
       <h2>{props.title}</h2>
+      <p>順番は{props.order}です。</p>
     </React.Fragment>
-  )
-}
+  );
+};
 
-export default Article
+export default Article;
--- src/Blog.jsx
+++ src/Blog.jsx
@@ -6,13 +6,16 @@ class Blog extends React.Component {
     super(props);
   }
   render() {
+    const authorName = "Torahack"
     return (
       <>
-        <Article title={"トラハック"}/>
+        <Article title={"Reactの使い方"} order={1}/>
+        <Article title={"JSXの使い方"} order={2}/>
+        <Article title={"環境構築をしてみよう"} order={3}/>
       </>
 
     )
-  }
-}
+  };
+};
 
-export default Blog
+export default Blog;
